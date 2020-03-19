647ee52533a210188915f6577f07b8c3341f9cba
tomotomo0511 <tomokaru0511@gmail.com>
2020-03-19 12:24:41 +0900

    fix repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.12.1"
 
 set :application, "PEP"
-set :repo_url, "git@example.com:me/PEP.git"
+set :repo_url, "git@github.com:tomotomo0511/PEP.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
