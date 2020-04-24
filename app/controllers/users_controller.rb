269abdcd3ced31ac4df056a73267fe337cfc628d
tomotomo0511 <tomokaru0511@gmail.com>
2020-04-24 23:46:07 +0900

    user_show_page_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order("created_at DESC").page(params[:page]).per(10).limit(3)
+    @posts = @user.posts.order("created_at DESC").page(params[:page]).per(10)
   end
 end
